b6b1609bbb815b5600836b5892e2e7340f114711
LastLoper <ys.mobility@gmail.com>
2022-11-29 15:40:23 +0900

    first commit

---
+++ BlogSearch/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlogSearch
+//
+//  Created by WalterCho on 2022/11/29.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
